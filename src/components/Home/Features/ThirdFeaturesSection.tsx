9055ece3836307fd8b83d3be9e2387d7cd6ac8b9
EhsanulSakib <ehsanulsakib.professional@gmail.com>
2025-03-20 15:24:49 +0600

    updated

--- src/components/Home/Features/ThirdFeaturesSection.tsx
+++ src/components/Home/Features/ThirdFeaturesSection.tsx
@@ -10,14 +10,14 @@ const ThirdFeaturesSection = () => {
         <div className='w-full'>
             <SectionTitle title={title} subTitle={subTitle} />
             <section className='mt-8 lg:mt-16 w-full flex flex-col md:flex-row justify-between items-center gap-4 lg:gap-8'>
-                <div className='min-h-[500px] w-full md:w-4/5 flex flex-col p-6 bg-gradient-to-tl from-tertiary/10 to-primary/40 rounded-xl'>
+                <div className='min-h-[500px] w-full md:w-4/5 flex flex-col p-6 bg-gradient-to-tl from-tertiary/10 to-background/40 rounded-xl'>
                     <div className='flex gap-2 items-center'>
                         <PiSquaresFour className='text-3xl w-10 lg:w-12 h-10 lg:h-12 bg-primary text-primaryText p-2 rounded-full' />
                         <h3 className='font-bold'>AIO SOLUTIONS</h3>
                     </div>
 
                     <div>
-                        <Image src={'/asset/aio.png'} alt="automation" width={1200} height={1200} className='w-64 mx-auto py-8 rounded-xl' />
+                        <Image src={'/asset/1.png'} alt="automation" width={1200} height={1200} className='w-64 mx-auto py-8 rounded-xl' />
                     </div>
 
                     <div>
@@ -27,10 +27,10 @@ const ThirdFeaturesSection = () => {
 
                 </div>
 
-                <div className='min-h-[500px] w-full flex flex-col p-6 bg-gradient-to-tl from-tertiary/10 to-primary/40 rounded-xl'>
+                <div className='min-h-[500px] w-full flex flex-col p-6 bg-gradient-to-tl from-tertiary/10 to-background/40 rounded-xl'>
                     <div className='flex gap-2 items-center'>
                         <PiSquaresFour className='text-3xl w-10 lg:w-12 h-10 lg:h-12 bg-primary text-primaryText p-2 rounded-full' />
-                        <h3 className='font-bold'>AIO SOLUTIONS</h3>
+                        <h3 className='font-bold'>CHATBOTS</h3>
                     </div>
 
                     <div>
@@ -38,16 +38,16 @@ const ThirdFeaturesSection = () => {
                     </div>
 
                     <div className='pr-6'>
-                        <h3 className='font-bold'>Customizable Solutions</h3>
-                        <p className='text-primaryText/70'>Tailor Your Experience with Our Flexible Features Designed to Meet Your Unique Needs!</p>
+                        <h3 className='font-bold'>Intelligent Chatbots</h3>
+                        <p className='text-primaryText/70'>Empowering Teams with Instant Assistance Across Modules and Customer Interactions!</p>
                     </div>
                 </div>
             </section>
             <section className='mt-4 lg:mt-8 w-full flex flex-col md:flex-row-reverse justify-between items-center gap-4 lg:gap-8'>
-                <div className='min-h-[500px] w-full md:w-4/5 flex flex-col p-6 bg-gradient-to-tl from-tertiary/10 to-primary/40 rounded-xl'>
+                <div className='min-h-[500px] w-full md:w-4/5 flex flex-col p-6 bg-gradient-to-tl from-tertiary/10 to-background/40 rounded-xl'>
                     <div className='flex gap-2 items-center'>
                         <PiSquaresFour className='text-3xl w-10 lg:w-12 h-10 lg:h-12 bg-primary text-primaryText p-2 rounded-full' />
-                        <h3 className='font-bold'>AIO SOLUTIONS</h3>
+                        <h3 className='font-bold'>INTEGRATIONS</h3>
                     </div>
 
                     <div>
@@ -55,16 +55,16 @@ const ThirdFeaturesSection = () => {
                     </div>
 
                     <div>
-                        <h3 className='font-bold'>Customizable Solutions</h3>
-                        <p className='text-primaryText/70'>Tailor Your Experience with Our Flexible Features Designed to Meet Your Unique Needs!</p>
+                        <h3 className='font-bold'>Seamless Integrations</h3>
+                        <p className='text-primaryText/70'>Connect Effortlessly with Leading Platforms for a Unified Workflow Experience with Hassel Free System!</p>
                     </div>
 
                 </div>
 
-                <div className='min-h-[500px] w-full flex flex-col p-6 bg-gradient-to-tl from-tertiary/10 to-primary/40 rounded-xl'>
+                <div className='min-h-[500px] w-full flex flex-col p-6 bg-gradient-to-tl from-tertiary/10 to-background/40 rounded-xl'>
                     <div className='flex gap-2 items-center'>
                         <PiSquaresFour className='text-3xl w-10 lg:w-12 h-10 lg:h-12 bg-primary text-primaryText p-2 rounded-full' />
-                        <h3 className='font-bold'>AIO SOLUTIONS</h3>
+                        <h3 className='font-bold'>TECHNOLOGY</h3>
                     </div>
 
                     <div>
@@ -72,8 +72,8 @@ const ThirdFeaturesSection = () => {
                     </div>
 
                     <div className='pr-6'>
-                        <h3 className='font-bold'>Customizable Solutions</h3>
-                        <p className='text-primaryText/70'>Tailor Your Experience with Our Flexible Features Designed to Meet Your Unique Needs!</p>
+                        <h3 className='font-bold'>Advanced Technology Utilization</h3>
+                        <p className='text-primaryText/70'>LEverage a Vast Array of Cutting-Edge Technologies to Enhance Productivity and Efficiency!</p>
                     </div>
                 </div>
             </section> 
